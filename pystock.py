917c9d9cb628d43b6ce6ff65982a004008a0a187
cody-shearer <cody.a.shearer@gmail.com>
2021-02-24 21:00:29 -0800

    Removed unknown user posts

--- pystock.py
+++ pystock.py
@@ -35,25 +35,27 @@ for s in subreddits:
     subreddit = reddit.subreddit(s)   # Chosing the subreddit
 
     for submission in subreddit.top(time_filter = 'day'):        
-        found_tickers = regex.findall(submission.title + ' ' + submission.selftext )
-        if 0 < len(found_tickers) < 200:
-            for ticker in found_tickers:
-                data.at[ticker, 'Total Mentions'] += 1
-                data.at[ticker, 'Score Weighted Mentions'] += submission.score
-                if submission.author.name not in data.at[ticker, 'Unique Users']:
-                    data.at[ticker, 'Unique Users'] += submission.author.name + '|'
-                    data.at[ticker, 'Unique Mentions'] += 1
+        found_tickers = regex.findall(submission    .title + ' ' + submission.selftext )
+        if hasattr(submission.author, 'name'):
+            if 0 < len(found_tickers) < 200:
+                for ticker in found_tickers:
+                    data.at[ticker, 'Total Mentions'] += 1
+                    data.at[ticker, 'Score Weighted Mentions'] += submission.score
+                    if submission.author.name not in data.at[ticker, 'Unique Users']:
+                        data.at[ticker, 'Unique Users'] += submission.author.name + '|'
+                        data.at[ticker, 'Unique Mentions'] += 1
 
         ##### Acessing comments on the post
         submission.comments.replace_more()
         for comment in submission.comments.list():
             found_tickers = regex.findall(comment.body)
-            if 0 < len(found_tickers) < 200:
-                for ticker in found_tickers:
-                    data.at[ticker, 'Total Mentions'] += 1
-                    data.at[ticker, 'Score Weighted Mentions'] += comment.score
-                    if comment.author.name not in data.at[ticker, 'Unique Users']:
-                        data.at[ticker, 'Unique Users'] += comment.author.name + '|'
-                        data.at[ticker, 'Unique Mentions'] += 1
+            if hasattr(comment.author, 'name'):
+                if 0 < len(found_tickers) < 200:
+                    for ticker in found_tickers:
+                        data.at[ticker, 'Total Mentions'] += 1
+                        data.at[ticker, 'Score Weighted Mentions'] += comment.score
+                        if comment.author.name not in data.at[ticker, 'Unique Users']:
+                            data.at[ticker, 'Unique Users'] += comment.author.name + '|'
+                            data.at[ticker, 'Unique Mentions'] += 1
     
-data.to_csv('subreddit_stock_data' + datetime.date.today() + '.csv')
+data.to_csv('subreddit_stock_data' + str(datetime.date.today()) + '.csv')
